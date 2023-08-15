56684368d7c217f81d25c0fb5915a3f088ae7aa2
parikshitRoy <parikshitroy00@gmail.com>
2023-08-15 12:23:28 +0530

    comment functionality completed

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@
 import User from "@/lib/models/user.model";
 import { connectToDB } from "@/lib/mongoose";
 import { revalidatePath } from "next/cache";
+import Thread from "@/lib/models/thread.modal";
 
 interface Params {
   userId: string;
@@ -33,7 +34,7 @@ export async function updateUser({
         path,
         onboarded: true,
       },
-      { upsert: true }
+      { upsert: true },
     );
     if (path === "/profile/edit") {
       revalidatePath(path);
@@ -51,5 +52,32 @@ export async function fetchUser(userId: string) {
     /* .populate({ path: 'communities', model:Community }); */
   } catch (error: any) {
     throw new Error(`Failed to fetch user: ${error.message}`);
+  }
+}
+
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB();
+
+    // Find all threads authored by user with the given UserId
+
+    //TODO: Populate community
+    const threads = await User.findOne({ id: userId }).populate({
+      path: "threads",
+      model: Thread,
+      populate: {
+        path: "children",
+        model: Thread,
+        populate: {
+          path: "author",
+          model: User,
+          select: "name image id",
+        },
+      },
+    });
+
+    return threads;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch User Post: ${error.message}`);
   }
 }
